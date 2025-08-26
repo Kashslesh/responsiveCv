8352419f6bd531fa58ecdb666f9eadb41258de89
Ivan <titovivanfr@gmail.com>
2025-08-26 10:15:48 +0200

    refactoring

--- javascript/app.js
+++ javascript/app.js
@@ -1,60 +1,59 @@
-'use strict';
+"use strict";
 //=============== declaration des variables=========================
-const tabs = document.querySelectorAll('.nav__link');
-const startIntersection = document.querySelector('.home__container');
-const scrollTop = document.getElementById('scroll-top');
+const tabs = document.querySelectorAll(".nav__link");
+const startIntersection = document.querySelector(".home__container");
+const scrollTop = document.getElementById("scroll-top");
 
 /*==================== SHOW MENU ====================*/
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId);
-    const nav = document.getElementById(navId);
-    
-    // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            // We add the show-menu class to the div tag with the nav__menu class
-            nav.classList.toggle('show-menu');
-        });
-    };
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+
+  // Validate that variables exist
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      // We add the show-menu class to the div tag with the nav__menu class
+      nav.classList.toggle("show-menu");
+    });
+  }
 };
-showMenu('nav-toggle','nav-menu');
+showMenu("nav-toggle", "nav-menu");
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 /*==================== REMOVE MENU MOBILE ====================*/
-document.querySelector('.nav__list').addEventListener('click', function(e){
-    e.preventDefault();
-    const navMenu = document.getElementById('nav-menu')
-    
-    if(e.target.classList.contains('nav__link')){
-        const id = e.target.getAttribute('href');
+document.querySelector(".nav__list").addEventListener("click", function (e) {
+  e.preventDefault();
+  const navMenu = document.getElementById("nav-menu");
 
-        const clicked = e.target.closest('.nav__link');
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
 
-        document.querySelector(id).scrollIntoView();
-        navMenu.classList.remove('show-menu');
-        
-         // guard clause
-        if(!clicked) return;
+    const clicked = e.target.closest(".nav__link");
 
-        // Remove active classes
-        tabs.forEach(tab => tab.classList.remove('active-link'));
-        // Activate tab
-        clicked.classList.add('active-link');
-    };
+    document.querySelector(id).scrollIntoView();
+    navMenu.classList.remove("show-menu");
+
+    // guard clause
+    if (!clicked) return;
+
+    // Remove active classes
+    tabs.forEach((tab) => tab.classList.remove("active-link"));
+    // Activate tab
+    clicked.classList.add("active-link");
+  }
 });
 
+/*==================== SHOW SCROLL TOP ====================*/
 
-/*==================== SHOW SCROLL TOP ====================*/ 
+const styckyArrow = function (entries) {
+  const [entry] = entries;
 
-const styckyArrow = function(entries){
-    const [entry] = entries;
-    
-    if(!entry.isIntersecting) scrollTop.classList.add('show-scroll');
-    else scrollTop.classList.remove('show-scroll');
-  }
+  if (!entry.isIntersecting) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+};
 
-const startObserver = new IntersectionObserver(styckyArrow,{
-    root:null,
-    threshold:.65,
-})
+const startObserver = new IntersectionObserver(styckyArrow, {
+  root: null,
+  threshold: 0.65,
+});
 startObserver.observe(startIntersection);
